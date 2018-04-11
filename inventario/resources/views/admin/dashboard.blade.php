13146f75921d85d22e107c88cb76168af24e9fec
Gabriel Medina <gabicho.edu@gmail.com>
2018-04-11 01:58:19 -0500

    subiendo dash

---
+++ inventario/resources/views/admin/dashboard.blade.php
@@ -0,0 +1,7 @@
+@extends('admin.layout')
+
+@section('content')
+
+<h1>DASHBOARD</h1>
+
+@stop
---
+++ inventario/resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="{{ asset ('plugins/bootstrap/css/bootstrap.css') }}">
+  </head>
+  <body>
+    <h1>esto es prueba</h1>
+    <a href="" class="btn btn-success">botoon</a>
+
+  </body>
+</html>
